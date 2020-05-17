32d1a0d16ece9bf16701ec655fb8de9018ef8391
김영재 <kyj141100@naver.com>
2020-05-17 14:08:51 +0900

    Add to view google search

--- app/src/main/java/com/github/freddyyj/foodsearcher/MainActivity.kt
+++ app/src/main/java/com/github/freddyyj/foodsearcher/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.github.freddyyj.foodsearcher
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
+import com.github.freddyyj.foodsearcher.ui.main.PageViewModel
+import com.github.freddyyj.foodsearcher.ui.main.ResultFragment
 import com.github.freddyyj.foodsearcher.ui.main.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +23,21 @@ class MainActivity : AppCompatActivity() {
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
 
+        tabs.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabReselected(p0: TabLayout.Tab?) {}
+            override fun onTabUnselected(p0: TabLayout.Tab?) {}
+            override fun onTabSelected(p0: TabLayout.Tab?) {
+                if (p0 != null && p0.position==1) {
+                    val result=sectionsPagerAdapter.getItem(0)
+                    val viewModel = ViewModelProviders.of(result).get(PageViewModel::class.java)
+                    Log.println(Log.WARN,"uri warn","URI has ${viewModel.recognizedText}")
+                    val url= Uri.parse("https://www.google.com/search?q=${viewModel.recognizedText}")
+                    val intent = Intent(Intent.ACTION_VIEW, url)
+                    startActivity(intent)
+                }
+            }
 
+        })
     }
 
 }
--- app/src/main/java/com/github/freddyyj/foodsearcher/ui/main/ResultFragment.kt
+++ app/src/main/java/com/github/freddyyj/foodsearcher/ui/main/ResultFragment.kt
@@ -1,13 +1,15 @@
 package com.github.freddyyj.foodsearcher.ui.main
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProviders
 import com.github.freddyyj.foodsearcher.R
-import kotlinx.android.synthetic.main.fragment_result.*
 
 
 /**
@@ -20,13 +22,13 @@ class ResultFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(PageViewModel::class.java)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        viewModel = ViewModelProviders.of(this).get(PageViewModel::class.java)
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_result, container, false)
